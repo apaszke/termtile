de4406e188c796812468d84f5d8812e1d4e463e8
Adam Paszke <adam.paszke@gmail.com>
2015-08-22 13:15:31 +0200

    Use FLT_EPSILON for comparing floats

--- src/getScreenInfo.m
+++ src/getScreenInfo.m
@@ -23,11 +23,11 @@ void printScreenInfo() {
         CGFloat diffWidth = screenRect.size.width - screenVisibleRect.size.width;
         diffHeight -= index == 0 ? MENU_HEIGHT : 0;
 
-        if (diffHeight != 0) {
+        if (fabs(diffHeight) > FLT_EPSILON) {
             dockScreenIndex = index;
             offsetBottom = diffHeight;
             break;
-        } else if (diffWidth != 0) {
+        } else if (fabs(diffWidth) > FLT_EPSILON) {
             dockScreenIndex = index;
             if (screenVisibleRect.origin.x - screenRect.origin.x > 0) {
                 offsetLeft = diffWidth;
